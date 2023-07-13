b91ab709e7631ad9d837e7452dc01090e6627f8f
Bianca Vieira <bncvr@outlook.com>
2023-07-12 21:57:03 -0300

    refactor HabitYearView

--- src/components/HabitYearView.js
+++ src/components/HabitYearView.js
@@ -8,51 +8,44 @@ const MAX_COLUMNS = 32;
 
 const HabitYearView = ({ dataSet, habitColor }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          {Array.from({ length: MAX_COLUMNS }, (_, index) => (
-            <th key={index} />
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {monthNames.map((month, monthIndex) => {
-          const days = getDaysInMonth(monthIndex, 2023);
+    <>
+      {monthNames.map((month, monthIndex) => {
+        const days = getDaysInMonth(monthIndex, 2023);
 
-          return (
-            <tr key={month}>
-              <td>
-                <P bold className="whitespace-nowrap font-semibold mt-2 mr-2">
-                  {month}
-                </P>
-              </td>
+        return (
+          <div key={month} className="flex mb-1 items-center">
+            <P
+              bold
+              className="whitespace-nowrap font-semibold mt-2 mr-2 min-w-[25px]"
+            >
+              {month}
+            </P>
+            <div className="flex flex-wrap">
               {Array.from({ length: MAX_COLUMNS - 1 }, (_, index) => {
                 const currentDate = new Date(2023, monthIndex, index + 1);
 
-                return (
-                  <td key={index}>
-                    {index < days.length && (
-                      <div
-                        title={formatDate(currentDate)}
-                        className="rounded w-6 h-6 border border-slate-500 hover:border-slate-200"
-                        style={{
-                          backgroundColor: dataSet.has(
-                            currentDate.toLocaleDateString()
-                          )
-                            ? habitColor || DEFAULT_HABIT_COLOR
-                            : "unset",
-                        }}
-                      />
-                    )}
-                  </td>
-                );
+                if (index < days.length) {
+                  return (
+                    <div
+                      key={index}
+                      title={formatDate(currentDate)}
+                      className="rounded w-6 h-6 border border-slate-500 hover:border-slate-200 mb-1 mr-1"
+                      style={{
+                        backgroundColor: dataSet.has(
+                          currentDate.toLocaleDateString()
+                        )
+                          ? habitColor || DEFAULT_HABIT_COLOR
+                          : "unset",
+                      }}
+                    />
+                  );
+                }
               })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
